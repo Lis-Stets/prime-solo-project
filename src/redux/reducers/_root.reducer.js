e9f20a240c0c8ab9c5e0a3300da50d99e1cfa321
Lisette Stetson <lisette.stetson@gmail.com>
2022-07-24 14:35:00 -0400

    itemsReducer added to root reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import closetReducer from './closet.reducer';
 import binReducer from './bin.reducer';
+import itemsReducer from './items.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   closetReducer,
   binReducer,
+  itemsReducer,
 });
 
 export default rootReducer;
